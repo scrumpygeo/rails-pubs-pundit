ee0b2af81eac9fd5cc5948cd4e01309583ba07f3
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-04-30 16:41:30 +0100

    add index show and create controllers

--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -1,2 +1,37 @@
 class PubsController < ApplicationController
+  before_action :find_pub, only: [:show, :create, :update, :destroy ]
+
+  def index
+    @pubs = Pub.all
+  end
+
+  def show
+  end
+
+  def new
+    @pub = Pub.new
+  end
+
+  def create
+    @pub = Pub.new(pub_params)
+    @pub.user = current_user
+    if @pub.save
+      redirect_to pubs_path
+    else 
+      render :new
+    end
+
+  end
+
+
+  private
+
+  def find_pub
+    @pub = Pub.find(params[:id])
+  end
+
+  def pub_params
+    params.require(:pub).permit(:name, :address)
+  end
+
 end
