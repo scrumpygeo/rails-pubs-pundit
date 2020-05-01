80ee859cda396cbf068ec9c861f101dc47cdd5e8
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-05-01 12:52:42 +0100

    add admin

--- app/policies/pub_policy.rb
+++ app/policies/pub_policy.rb
@@ -14,17 +14,17 @@ class PubPolicy < ApplicationPolicy
   end
 
   def update?
-    user_is_owner?
+    user_is_owner_or_admin?
   end
 
   def destroy?
-    user_is_owner?
+    user_is_owner_or_admin?
   end
 
   private
 
-  def user_is_owner?
-    record.user == user
+  def user_is_owner_or_admin?
+    record.user == user || user.admin
   end
 
 
